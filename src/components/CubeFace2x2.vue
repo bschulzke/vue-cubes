046737aa442d512ef8b78f2288f252172b77a7ff
Ben Schulzke <benschulzke@gmail.com>
2023-11-29 15:14:13 -0700

    implement swipe moves for front, back, left, right

--- src/components/CubeFace2x2.vue
+++ src/components/CubeFace2x2.vue
@@ -2,14 +2,14 @@
     <div style="display: flex;">
         <div>
             <div 
-            ref="el"
+            ref="c0"
             @mousedown="$emit('start-swipe', 0)" 
             @mouseup="$emit('end-swipe', 0)"
             @touchstart="start = 0"
             :class="['square', face[0]]"
             />
             <div
-            ref="el3"
+            ref="c3"
             @mousedown="$emit('start-swipe', 3)"
             @mouseup="$emit('end-swipe', 3)"
             @touchstart="start = 3"
@@ -18,12 +18,14 @@
         </div>
         <div>
             <div 
+            ref="c1"
             @mousedown="$emit('start-swipe', 1)"
             @mouseup="$emit('end-swipe', 1)" 
             @touchstart="start = 1"
             :class="['square', face[1]]"
             />
             <div 
+            ref="c2"
             @mousedown="$emit('start-swipe', 2)"
             @mouseup="$emit('end-swipe', 2)"
             @touchstart="start = 2"
@@ -49,8 +51,8 @@ export default {
         }
     },
     setup(props, context) {
-    const el = ref(null)
-    const { isSwiping, direction } = useSwipe(el, {
+    const c0 = ref(null)
+    const { isSwiping, direction } = useSwipe(c0, {
         onSwipeEnd(e, direction) {
             context.emit('start-swipe', 0);
             if (direction === 'right') {
@@ -61,8 +63,32 @@ export default {
         }
     })
 
-    const el3 = ref(null)
-    const { isSwiping3, direction3 } = useSwipe(el3, {
+    const c1 = ref(null)
+    const { isSwiping1, direction1 } = useSwipe(c1, {
+        onSwipeEnd(e, direction) {
+            context.emit('start-swipe', 1);
+            if (direction === 'left') {
+                context.emit('end-swipe', 0);
+            } else if (direction === 'down') {
+                context.emit('end-swipe', 2);
+            }
+        }
+    })
+
+    const c2 = ref(null)
+    const { isSwiping2, direction2 } = useSwipe(c2, {
+        onSwipeEnd(e, direction) {
+            context.emit('start-swipe', 2);
+            if (direction === 'left') {
+                context.emit('end-swipe', 3);
+            } else if (direction === 'up') {
+                context.emit('end-swipe', 1);
+            }
+        }
+    })
+
+    const c3 = ref(null)
+    const { isSwiping3, direction3 } = useSwipe(c3, {
         onSwipeEnd(e, direction) {
             context.emit('start-swipe', 3);
             if (direction === 'right') {
@@ -73,7 +99,7 @@ export default {
         }
     })
 
-    return { el, isSwiping, direction, el3, isSwiping3, direction3 }
+    return { c0, isSwiping, direction, c1, isSwiping1, direction1, c2, isSwiping2, direction2, c3, isSwiping3, direction3 }
   }
 }
 </script>
